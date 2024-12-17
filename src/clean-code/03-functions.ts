c38198a1e177bc01c269d62f9157e9c10cf6f194
avsotomejia@gmail.com <avsotomejia@gmail.com>
2024-12-17 17:58:07 -0500

    add file 04-functions.ts

--- src/clean-code/03-functions.ts
+++ src/clean-code/03-functions.ts
@@ -1,34 +1,38 @@
 (() => {
 
-    // función para obtener información de una película por Id
     function getMovieById( id: string ) {
         console.log({ id });
     }
 
-    // función para obtener información de los actores de una película - Actors o Cast // id = movieId getMovieCast
-    function getMovieCast( id: string ) {
+    function getMovieCastById( id: string ) {
         console.log({ id });
     }
 
-    // funcion para obtener el bio del actor por el id
-    function getActorById( id: string ) {
+    function getActorBioById( id: string ) {
         console.log({ id });
     }
     
-    // Crear una película
-    function createMovie(title: string, description: string, rating: number, cast: string[] ) {
+    interface Movie{
+        cast:           string[];
+        description:    string;
+        rating:         number;
+        title:          string;
+    }
+
+    function createMovie({title, description, rating, cast }: Movie ) {
         console.log({ title, description, rating, cast });
     }
 
-    // Crea un nuevo actor
-    function createActor( fullName: string, birthdate: Date ): boolean {
-        
-        // tarea asincrona para verificar nombre
-        // ..
-        // ..
-        if ( fullName === 'fernando' ) return false;
+    function validateFullName( fullName: string){
+        console.log({ fullName });
+        return true;
+    }
 
-        console.log('Create actor');
+    function createActor( fullName: string, birthdate: Date ): boolean {
+    
+        if ( validateFullName(fullName)) return false;
+
+        console.log('Create actor', birthdate);
         return true;        
 
     }
